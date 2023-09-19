0eb5e5ad13f3b406ca2fb8fb87d002bbe06085b6
TomasTorres27 <tomitorres2797@gmail.com>
2023-09-19 12:53:42 -0300

    K

--- src/universidadejemplo/Entidades/UniversidadG40.java
+++ src/universidadejemplo/Entidades/UniversidadG40.java
@@ -7,16 +7,16 @@ public class UniversidadG40 {
 
     public static void main(String[] args) {
         AlumnoData alu = new AlumnoData();
-        Alumno alum= new Alumno(5, "Fiora", "Champ", LocalDate.of(2000, 10, 15), true);
+        Alumno alum= new Alumno(444,"Francisco", "Iorfino", LocalDate.of(1997, 5, 20), true);
         //alu.guardarAlumno(alum);
-        //alu.modificarAlumno(Luna);
+        //alu.modificarAlumno();
         //alu.eliminarAlumno(1);
         /*
         Alumno aluEnc = alu.buscarAlumno(1);
         if(aluEnc!=null){
           System.out.println("dni: "+aluEnc.getDni());
           System.out.println("Apellido: "+aluEnc.getApellido());
-            System.out.println("Nombre: "+aluEnc.getNombre());
+          System.out.println("Nombre: "+aluEnc.getNombre());
         }
         */
         /*for(Alumno alumno:alu.listarAlumnos()){
@@ -25,31 +25,26 @@ public class UniversidadG40 {
             System.out.println(alumno.getNombre());
             System.out.println(alumno.getFechaNacimiento());*/
             
-        MateriaData mater= new MateriaData();
-        Materia Mate = new Materia ("Literatura", 2020, true);
-        //mater.guardarMateria(Mate);
+//        MateriaData mater= new MateriaData();
+//        Materia Mate = new Materia ("Geografia", 2019, true);
+//        mater.guardarMateria(Mate);
         
         /*mater.eliminarMateria(2);*/
         AlumnoData ad= new AlumnoData();
         MateriaData md= new MateriaData();
         InscripcionData id= new InscripcionData();
-        Alumno al= ad.buscarAlumno(2);
-        Materia mat= md.buscarMateria(3);
+        Alumno al= ad.buscarAlumno(4);
+        Materia mat= md.buscarMateria(2);
         Inscripcion ins= new Inscripcion(al, mat, 7);
         //id.guardarInscripcion(ins);
-        //id.actualizarNota(2, 3, 6);
-        //id.borrarInscripcion(2, 3);
-        
-//        for(Inscripcion insc: id.obtenerInscripciones()){
-//            System.out.println("id"+insc.getIdInscripcion());
-//            System.out.println("Apellido"+insc.getAlumno().getApellido());
-//            System.out.println("Materia"+insc.getMateria().getNombre());
-//        }
-       
-//    for(Materia materia: id.obtenerMateriasNoCursadas(3)){
-//           System.out.println("nombre"+ materia.getNombre());
-//       }
-//    }
+        for(Inscripcion insc: id.obtenerInscripciones()){
+            System.out.println("id"+insc.getIdInscripcion());
+            System.out.println("Apellido: "+insc.getAlumno().getApellido());
+            System.out.println("Materia: "+insc.getMateria().getNombre());
+        }
+       for(Materia materia: id.obtenerMateriasNoCursadas(3)){
+           System.out.println("Nombre: "+ materia.getNombre());
+       }
     }
 }
 
